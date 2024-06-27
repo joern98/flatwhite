7b444aff84204490a1d3eca5629059f85f14062c
joern.e <joern.eggersgluess@gmail.com>
2024-06-27 22:41:35 +0200

    remove git installation from setup.sh

--- pi-setup/setup.sh
+++ pi-setup/setup.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get -y upgrade
 
 # install basic tools
-apt-get -y install vim git
+apt-get -y install vim 
 
 # install python dependencies
 apt-get -y install python3-pip python3-pil python3-numpy python3-RPi.GPIO python3-spidev python3-gpiozero
