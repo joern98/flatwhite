400894f6757bf09a4f80493045d5efe1d5cc73ae
joern <joern.eggersgluess@gmail.com>
2024-07-01 17:19:17 +0200

    refactor display

--- core/core.py
+++ core/core.py
@@ -1,4 +1,47 @@
 import logging
+import os
+import time
+
+from PIL import Image, ImageDraw, ImageFont
+
+from display.epd import EPD 
+
 logging.basicConfig(level=logging.DEBUG)
 
-from ui import Component, Layout
+RESOURCE_PATH = os.path.join(os.path.dirname(__file__), "..", "res")
+logging.debug(f"RESOURCE_PATH = {RESOURCE_PATH}")
+
+font18 = ImageFont.truetype(os.path.join(RESOURCE_PATH, "Font.ttc"), 18)
+
+def get_test_image_h(width, height):
+        # Horizontal image, epd.height is the long side with 276 pixels
+        # 264x176
+
+        image = Image.new('1', (width, height), 0xFF)
+        draw = ImageDraw.Draw(image)
+        draw.font = font18
+        draw.rectangle((0, 0, width-1, height-1), outline=0)
+        draw.text((0, 0), "(0, 0)", fill=0)
+        draw.text((width//2, height//2), "Center", fill=0, anchor="mm")
+        draw.text((0, height-1), "(0, 175)", fill=0, anchor="ld") 
+        draw.text((width-1, height-1), "(263, 175)", fill=0, anchor="rd") 
+        draw.text((width-1, 0), "(263, 0)", fill=0, anchor="ra") 
+        return image
+
+def main():
+    epd = EPD()
+
+    try:
+        image = get_test_image_h(epd.size[0], epd.size[1])
+        epd.show_image(image)
+
+        time.sleep(5)
+
+        epd.clear()
+        epd.clean()
+
+    except KeyboardInterrupt:
+        logging.warning("Keyboard interrupt, clear and exit")
+        epd.clear()
+        epd.clean()
+        exit(1) 
--- core/display/epd.py
+++ core/display/epd.py
@@ -11,11 +11,37 @@ from .display import Display
 
 class EPD(Display):
 
-    def __init__(self, size: Tuple[int, int]) -> None:
-        super().__init__(size)
-        self.epd = epd2in7_V2.EPD()
+    def __init__(self) -> None:
+        try:
+            self.epd = epd2in7_V2.EPD()
+        except Exception as e:
+            logging.critical("Failed to create EPD class!")
+            logging.debug(e)
 
-    def show_image(image: Image.Image):
-        epd.init_Fast()
-        epd.display_Fast(epd.getbuffer(image))
-        epd.sleep()
+        size = (self.epd.height, self.epd.width)
+        super().__init__(size)
+
+
+    @override()
+    def show_image(self, image: Image.Image):
+        try:
+            self.epd.init_Fast()
+            self.epd.display_Fast(self.epd.getbuffer(image))
+            self.epd.sleep()
+        except:
+            self.clean()
+            raise
+
+    @override()
+    def clear(self):
+        try:
+            self.epd.init()
+            self.epd.Clear()
+            self.epd.sleep()
+        except:
+            self.clean()
+            raise
+
+    def clean():
+        logging.debug("Calling module_exit() for cleanup")
+        epd2in7_V2.epdconfig.module_exit(cleanup=True)
