576aa096d93882dfe41a9bfa8ee01fddb5b6327a
joern.e <joern.eggersgluess@gmail.com>
2024-07-15 18:10:30 +0200

    basic integration with sonos

--- src/core.py
+++ src/core.py
@@ -3,13 +3,11 @@ import os
 import time
 import platform
 
-import soco
-from soco import events_twisted
-soco.config.EVENTS_MODULE = events_twisted
 from twisted.internet import reactor
-from PIL import Image, ImageDraw, ImageFont
 
+from .sonos import SonosService
 from .gui import *
+from .config import RESOURCE_PATH
 
 if platform.system() == "Linux":
     from .output import EPD as Output
@@ -18,7 +16,6 @@ elif platform.system() in ["Windows", "Darwin"]:
 
 logging.basicConfig(level=logging.DEBUG)
 
-RESOURCE_PATH = os.path.join(os.path.dirname(__file__), "..", "res")
 logging.debug(f"RESOURCE_PATH = {RESOURCE_PATH}")
 
 BLACK = 0x00
@@ -27,46 +24,34 @@ WHITE = 0xFF
 WIDTH = 264
 HEIGHT = 176
 
-font18 = ImageFont.truetype(os.path.join(RESOURCE_PATH, "Font.ttc"), 18)
-
-def get_test_image_h(width, height):
-        # Horizontal image, epd.height is the long side with 276 pixels
-        # 264x176
-
-        image = Image.new('1', (width, height), 0xFF)
-        draw = ImageDraw.Draw(image)
-        draw.font = font18
-        draw.rectangle((0, 0, width-1, height-1), outline=0)
-        draw.text((0, 0), "(0, 0)", fill=0)
-        draw.text((width//2, height//2), "Center", fill=0, anchor="mm")
-        draw.text((0, height-1), "(0, 175)", fill=0, anchor="ld") 
-        draw.text((width-1, height-1), "(263, 175)", fill=0, anchor="rd") 
-        draw.text((width-1, 0), "(263, 0)", fill=0, anchor="ra") 
-        return image
-
-def setup_sonos():
-     sonos = soco.SoCo("192.168.178.26")
-     logging.info(sonos.player_name)
-     logging.info(sonos.ip_address)
-     return sonos
+tb_title = None
+tb_artist = None
 
 def setup_gui(output):
+    global tb_title, tb_artist
+
+    PAD_X = 2
+    PAD_Y = 2
     b = GUI.builder()
     b.set_output(output)
     b.set_size(WIDTH, HEIGHT)
-    b.rectangle(0, 0, WIDTH-1, HEIGHT-1)
-    b.rectangle(WIDTH//3, HEIGHT//3, WIDTH-1, HEIGHT-1)
-    b.rectangle(2*WIDTH//3, 2*HEIGHT//3, WIDTH-1, HEIGHT-1)
-    t = b.textbox(2*WIDTH//3, 2*HEIGHT//3, WIDTH-1, HEIGHT-1, "Hello World, my ass is on fire up in here")
-    return b.build(), t
+    tb_title = b.textbox(PAD_X, PAD_Y, WIDTH-PAD_X-1, PAD_Y+49, "...")
+    tb_artist = b.textbox(PAD_X, PAD_Y+50, WIDTH-PAD_X-1, HEIGHT-PAD_Y-1, "...")
+    return b.build()
      
 def main():
     output = Output()
-    gui, t = setup_gui(output)    
-    gui.update()
-    time.sleep(2)
-    t.text = "This is the changed text, it is now different as fuck"
-    gui.update()
+    gui= setup_gui(output)    
+
+    sonos_service = SonosService()
+
+    def change_callback(title, artist):
+        global tb_title, tb_artist
+        tb_title.text = title
+        tb_artist.text = artist
+        gui.update()
+        
+    sonos_service.on_change(change_callback)
 
     def before_shutdown():
         output.clear()
--- src/sonos.py
+++ src/sonos.py
@@ -1,7 +1,10 @@
 import logging
 
 import soco
-from twisted.internet import reactor
+from soco import events_twisted
+soco.config.EVENTS_MODULE = events_twisted
+
+import json
 
 
 class SonosService:
@@ -13,6 +16,26 @@ class SonosService:
         except Exception as e:
             logging.error("Failed to connect to Sonos speaker with IP adress 192.168.178.26")
             raise e
+    
+        self.__current_track_uri = None
+
+        self.__on_change_callback = None
+
+        self.__sub_avTransport = self.__sonos.avTransport.subscribe().subscription
+        self.__sub_avTransport.callback = self.__avt_event_callback
+
+    def __avt_event_callback(self, event):        
+        if event.variables["current_track_uri"] == self.__current_track_uri:
+            return
+        self.__current_track_uri = event.variables["current_track_uri"]
+
+        track_meta_data = event.variables["current_track_meta_data"]
+        self.__on_change_callback(track_meta_data.title, track_meta_data.creator)
+
+    def on_change(self, callback):
+        self.__on_change_callback = callback
+
         
     
+
     
